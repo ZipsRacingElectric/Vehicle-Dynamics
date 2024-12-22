c06bb75929e2ef67d7839dfa19896a0eca5db43c
Brian Glen <br.glen@yahoo.com>
2024-12-21 23:13:58 -0500

    Updated nonlinear double track model

--- MATLAB/nonlinear_double_track/nonlinear_double_track.m
+++ MATLAB/nonlinear_double_track/nonlinear_double_track.m
@@ -11,14 +11,23 @@ TODO:
 - analyze tire data for more accurate relaxation length (has huge effects)
 - go through tire model and verify coordinate systems
 - validate and improve the vehicle parameters
+- IA is modeled as static -2 degrees, this should be pulled from the vehicle parameters 
 
 %}
 
 clear; close all; clc;
 Simulink.sdi.clear; % clear old simulink runs
 
-% load vehicle simulink parameters
-run("../vehicle_data/zr25.m");
+% ensure vehicle object in in matlab path
+directory = fileparts(which(mfilename)); % Get the directory of the current script
+parentDirectory = fileparts(directory); % Get the parent directory of the current script
+addpath(genpath(parentDirectory));      % Add the parent directory and all its subdirectories to the MATLAB path
+
+% load vehicle data
+zr25 = vehicle("../vehicle_data/zr25_data.xlsx");
+
+% create simulink parameters
+zr25.create_simulink_parameters();
 
 % load tire model
 load("../tire_data/d2704/d2704_7in_csaps.mat");
