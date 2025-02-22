76b55f9e1110889f0d85859f230ff687c9272f36
Brian Glen <br.glen@yahoo.com>
2025-02-22 15:53:49 -0500

    Yaw Rate Reference Results generation

--- MATLAB/yaw_rate_reference_generation/yaw_rate_ref_gen.m
+++ MATLAB/yaw_rate_reference_generation/yaw_rate_ref_gen.m
@@ -1,22 +1,27 @@
 %{
 
 %% Overview
-This script uses a linear single track vehicle model to derive an ideal
-yaw-rate from kinematic steering and velocity inputs. This ideal yaw-rate
-can then be used as a refernce value in a yaw rate feedback control system.
+This script uses a quasi-static single track vehicle model to derive an ideal
+yaw-rate from kinematic steering and velocity inputs. The yaw rate is valid 
+for quasi-static steady state conditions, which means it is an ideal target
+at corner apex, where the yaw moment is zero. This ideal yaw-rate can then 
+be used as a refernce value in a yaw rate feedback control system to 
+improve corner apex lateral acceleration, and thereforce overall corner
+speed and lower lap times.
 
 %% Details:
-It calulates this yaw rate based on an understeer gradiant K_u which is designed to achieve an
-ideal response. By setting K_u close to zero, the vehicle model experiences
-neutral steer, which gives the smallest turning radius for a given lateral
-acceleration. Therefore, for a fixed radius corner, the vehicle closest to
-neutral steer achieves the highest lateral acceleration. The yaw-rate
-associated with this condition is calculated and can then be the
-torque-vectoring controler's target.
+It calulates this yaw rate based on an understeer gradiant K_u which is 
+designed to achieve an ideal response. Using a linear model for this is 
+okay because it represents ideal vehicle behavior. By setting K_u close to
+zero, the vehicle model experiences neutral steer, which gives the smallest
+turning radius for a given lateral acceleration. Therefore, for a fixed
+radius corner, the vehicle closest to neutral steer achieves the highest
+lateral acceleration. The yaw-rate associated with this condition is
+calculated and can then be the torque-vectoring controller's target.
 
 %% TODO:
 -  update cornering stiffnesses with tire data
-- graph yaw rate using the target understeer gradient
+- graph steering angle as function of a_y
 
 %}
 
@@ -43,42 +48,138 @@ mu = 3; % max tire-road friction coefficient from TTC data
 % without compliance
 K_u = (zr25.mass_total / zr25.wheelbase) * ((zr25.b / (C_f * Fz_f)) -  (zr25.a / (C_r * Fz_r)));
 
-%% Graph Yaw Rate
+% This understeer gradient we will specifiy to be zero, which will design
+% the ideal yaw rate
+K_u_ref = 0;
+
+%% Yaw Rate vs Steering Angle
 v = [10, 20, 30, 40]; % Velocities in m/s
 delta_sw = linspace(0, 180, 100); % Steering wheel angle (degrees)
 delta = deg2rad(delta_sw .* 0.138); % Convert steering wheel angle to dynamic wheel angle
+yaw_rate = zeros(length(v), length(delta)); % yaw_rate using the linear understeer gradient
+yaw_rate_ref = zeros(length(v), length(delta)); % yaw_rate using the designed understeer gradient
 
+for i = 1:length(v)
+    % Calculate maximum yaw rate
+    yaw_rate_max = sigma * mu * zr25.g / v(i);
+
+    for u = 1:length(delta)
+
+        rate = delta(u) / (zr25.wheelbase + K_u * v(i)); % Y = delta_dyn / (l + K_u * V)
+        if (rate < yaw_rate_max)
+
+            yaw_rate(i, u) = rate;
+        else
+            yaw_rate(i, u) = yaw_rate_max;
+        end
+
+        rate_ref = delta(u) / (zr25.wheelbase + K_u_ref * v(i)); % Y = delta_dyn / (l + K_u_ref * V)
+        if (rate_ref < yaw_rate_max)
+
+            yaw_rate_ref(i, u) = rate_ref;
+        else
+            yaw_rate_ref(i, u) = yaw_rate_max;
+        end
+    end
+end
+
+%% Plot Yaw Rates
 line_styles = {'-','--',':','-.'}; 
 colors = lines(length(v));
+
 figure;
 hold on;
 grid on;
 xlabel("Steering Wheel Angle (°)", 'FontSize', 12, 'FontWeight', 'bold')
 ylabel("Yaw Rate (rad/s)", 'FontSize', 12, 'FontWeight', 'bold')
-title("Yaw Rate vs. Steering Wheel Angle for Linear Understeer Gradient", ...
+title("Yaw Rate vs. Steering Angle for K_u = ", K_u, ...
       'FontSize', 14, 'FontWeight', 'bold')
 legend_entries = cell(1, length(v));
-
 for i = 1:length(v)
-    
-    % Calculate maximum yaw rate
-    yaw_rate_max = sigma * mu * zr25.g / v(i);
-
-    % Calculate yaw rate
-    yaw_rate = zeros(1, length(delta));
-    for u = 1:length(delta)
-        rate = delta(u) / (zr25.wheelbase + K_u * v(i)); % Y = delta_dyn / (l + K_u * V)
-        if (rate < yaw_rate_max)
-            yaw_rate(u) = rate;
-        else
-            yaw_rate(u) = yaw_rate_max;
-        end
-    end
-
-
-    plot(delta_sw, yaw_rate, 'LineWidth', 2, 'LineStyle', line_styles{i}, 'Color', colors(i, :))
+    plot(delta_sw, yaw_rate(i, :), 'LineWidth', 2, 'LineStyle', line_styles{i}, 'Color', colors(i, :))
     legend_entries{i} = sprintf("V = %d m/s", v(i));
 end
+legend(legend_entries, 'Location', 'best', 'FontSize', 10)
+hold off;
 
+figure;
+hold on;
+grid on;
+xlabel("Steering Wheel Angle (°)", 'FontSize', 12, 'FontWeight', 'bold')
+ylabel("Yaw Rate (rad/s)", 'FontSize', 12, 'FontWeight', 'bold')
+title("Yaw Rate Reference vs. Steering Angle for K_u = ", K_u_ref, ...
+      'FontSize', 14, 'FontWeight', 'bold')
+legend_entries = cell(1, length(v));
+for i = 1:length(v)
+    plot(delta_sw, yaw_rate_ref(i, :), 'LineWidth', 2, 'LineStyle', line_styles{i}, 'Color', colors(i, :))
+    legend_entries{i} = sprintf("V = %d m/s", v(i));
+end
+legend(legend_entries, 'Location', 'best', 'FontSize', 10)
+hold off;
+
+%% Steering Angle vs Ay
+
+% Note: these equations to solve for steer vs Ay need to be confirmed. It
+% seems like there are multiple different solutions floating around on the
+% internet
+
+% delta_dyn = delta_kin + K_u * a_y
+% a_y = V^2 / R -> R = V^2 / a_y
+% delta_kin = L / R = L * a_y / V^2
+% delta_dyn = (L * a_y / V^2) + K_u * a_y
+a_y = linspace(0, 3, 99) * zr25.g;
+
+delta_dyn = zeros(length(v), length(a_y)); % steered angle using the linear understeer gradient
+delta_dyn_ref = zeros(length(v), length(a_y)); % steered angle using the designed understeer gradient
+
+for i = 1:length(v)
+    % Calculate maximum a_y
+    a_y_max = sigma * mu * zr25.g;
+
+    for u = 1:length(a_y)
+
+        angle = (zr25.wheelbase * a_y(u) / v(i)^2) + K_u * a_y(u);
+        if (a_y(u) < a_y_max)
+
+            delta_dyn(i, u) = (zr25.wheelbase * a_y(u) / v(i)^2) + K_u * a_y(u);
+            delta_dyn_ref(i, u) = (zr25.wheelbase * a_y(u) / v(i)^2) + K_u_ref * a_y(u);
+        else
+            delta_dyn(i, u) = NaN;
+            delta_dyn_ref(i, u) = NaN;
+        end
+    end
+end
+
+%% Plot Steering Angle vs Ay
+line_styles = {'-','--',':','-.'}; 
+colors = lines(length(v));
+
+figure;
+hold on;
+grid on;
+xlabel("Lateral Acceleration Ay, (m/s^2)", 'FontSize', 12, 'FontWeight', 'bold')
+ylabel("Steering Angle (rad)", 'FontSize', 12, 'FontWeight', 'bold')
+title("Steering Angle vs A_y for K_u = ", K_u, ...
+      'FontSize', 14, 'FontWeight', 'bold')
+legend_entries = cell(1, length(v));
+for i = 1:length(v)
+    plot(a_y, delta_dyn(i, :), 'LineWidth', 2, 'LineStyle', line_styles{i}, 'Color', colors(i, :))
+    legend_entries{i} = sprintf("V = %d m/s", v(i));
+end
+legend(legend_entries, 'Location', 'best', 'FontSize', 10)
+hold off;
+
+figure;
+hold on;
+grid on;
+xlabel("Lateral Acceleration Ay, (m/s^2)", 'FontSize', 12, 'FontWeight', 'bold')
+ylabel("Steering Angle (rad)", 'FontSize', 12, 'FontWeight', 'bold')
+title("Steering Angle vs A_y for K_u_ref = ", K_u_ref, ...
+      'FontSize', 14, 'FontWeight', 'bold')
+legend_entries = cell(1, length(v));
+for i = 1:length(v)
+    plot((a_y ./ zr25.g), delta_dyn_ref(i, :), 'LineWidth', 2, 'LineStyle', line_styles{i}, 'Color', colors(i, :))
+    legend_entries{i} = sprintf("V = %d m/s", v(i));
+end
 legend(legend_entries, 'Location', 'best', 'FontSize', 10)
 hold off;
