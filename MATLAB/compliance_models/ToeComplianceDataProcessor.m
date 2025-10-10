b2f9cbbfe31dd9212487fedd046ec34dc3c492e4
Ben Model <benmodel553@gmail.com>
2025-10-10 15:31:33 -0400

    polishing, fix unit conversions, move functions to end
    Needed to move functions to the end for it to run. Changed file path to work universally as long as working directory is properly set to VehicleDynamics/MATLAB (check with (pwd). Timeless mass vs weight debate, kgf. dial indicators in thousandths, convert inches to meters for toe base.

--- MATLAB/compliance_models/ToeComplianceDataProcessor.m
+++ MATLAB/compliance_models/ToeComplianceDataProcessor.m
@@ -1,5 +1,4 @@
 
-
 % This Toe Compliance calculator uses deflection and moment measurements to
 % calculate the toe compliance in degrees/Nm
 
@@ -21,7 +20,7 @@ clear, clc
 % and automatically detects headers making it easy to call values or
 % specific index values
 
-Excel = readtable("C:\Users\ATuck\Documents\GitHub\Vehicle-Dynamics\MATLAB\compliance_models\Compliance Testing ZR25.xlsx")
+Excel = readtable("\compliance_models\Compliance Testing ZR25.xlsx")
 
 %% 2. Calculate moments
 % To calculate our applied moments we need to know two variables. our lever
@@ -31,18 +30,18 @@ Excel = readtable("C:\Users\ATuck\Documents\GitHub\Vehicle-Dynamics\MATLAB\compl
 % length never changed and the values is defined in the excel table. We can
 % start by assinging this value a variable name.
 
-dLeverArm = 1 % Lever arm length in meters.
+dLeverArm = 1; % Lever arm length in meters.
 
 % This is when we need to start being aware of what units we are working
 % with. We know our final output should be in degrees/Nm. Because of this
 % our distance values should all be in meters, our angle values should
 % be in degrees, and our force values in newtons.
 
-% Next we need to define our forces. Our mass values are stored in a column titled Kg
+% Next we need to define our forces. Our force values are stored in a column titled Kg
 % We can load the four differnet load cases into a matrix under one
 % variable name.
 
-Mass = Excel.kg(1:4)
+forcesApplied = Excel.kg(1:4)
 
 % This is an alternative way to call the same values by indexing their
 % cells. Mass = Excel{1:4,1} 1:4 means rows 1 through 4 the , 1 pulls
@@ -50,20 +49,7 @@ Mass = Excel.kg(1:4)
 
 % Now we are ready to find our moments. Lets use a function for this. 
 
-ToeMoments = GetMoment(Mass,dLeverArm)
-
-%% moment function
-% This function needs fed mass (Kg) and lever arm length (LAD)
-% We start by writing what the output will be, in this case "moments".
-% Next we define the name of our function "GetMoment" and put the
-% arguements it will need in  parenthesis (FKg, LAD)
-
-function moments = GetMoment(Kg, LAD) % get moments back in Nm
-
-% We then write the driving equation using the variables defined in the title. 
-    moments = Kg*LAD*9.81 % {kg*m*m/s^2) = Nm
-
-end
+ToeMoments = GetMoment(forcesApplied,dLeverArm)
 
 %% 3. Find displacements of dial indicators
 % to find the diplacements we need to calculate the difference between the
@@ -76,28 +62,21 @@ end
 % First, assign variable names to the values of each dial indicator. The
 % naming convention will be DI1 (displacement indicator one) etc...
 
-DI1 = Excel{1:4,2}
-DI2 = Excel{1:4,3}
-DI3 = Excel{1:4,4}
-DI4 = Excel{1:4,5}
+Dial1 = Excel{1:4,2};
+Dial2 = Excel{1:4,3};
+Dial3 = Excel{1:4,4};
+Dial4 = Excel{1:4,5};
 
-% In the excel file these values were recorded in hundredths of an inch. We
+% In the excel file these values were recorded in thousandths of an inch. We
 % need to convert this to meters within our displacements function
 % lets call the function and build our two, [4x1] matrices
 
-DR = GetDisplacements(DI1, DI2) % displacement at corner
-DL = GetDisplacements(DI3, DI4) % displacement at adjacent corner
+DisplacementR = GetDisplacements(Dial1, Dial2) % displacement at corner
+DisplacementL = GetDisplacements(Dial3, Dial4) % displacement at adjacent corner
 
-% Using a function here has saved us 16 calculations
+% Using a function here has saved us 16 lines of calculations
 
-%% diaplacement function
-function distance = GetDisplacements(D1in, D2in)
 
-distance = D1in-D2in
-% convert to meters
-distance = distance*.000254 % from hundredths to meters
-
-end
 
 %% 4. Find Toe Angles
 % we almost have all the pieces we need to find our compliance. We have the
@@ -110,40 +89,27 @@ end
 % the second tringle side.This was measured in inches so must also be
 % converted to meters.
 
-d = Excel{3,6} 
+d = Excel{3,6};
 
 % this pulls a single cell which can't easily be divided with the 4x1
 % displacement matrices. We'll have to account for this in our function
              
-DY = str2double(d{1});       % convert '15.5' → 15.5 numeric
+toeBase = str2double(d{1}) * 0.0254;       % convert '15.5' → 15.5 numeric
 
 
 % now we can solve for the angles at each force interval. Naming convention
 % toe angle on right side = TARS
-TARS = GetCompAng(DY, DR); % corner side theta calc
-TALS = GetCompAng(DY, DL); % adjacent side theta calc
-
-%% Compliance angle function
-function theta = GetCompAng(Dispx,Dispy)
-
-theta = rad2deg(atan(Dispy./Dispx)); 
-% here we use ./ to divide each element of the matrix by the scalar. 
+TARS = GetComplianceAng(toeBase, DisplacementR); % corner side theta calc
+TALS = GetComplianceAng(toeBase, DisplacementL); % adjacent side theta calc
 
 
-end
 %% Find compliance for each side (degrees per Nm) 
 % we can divide the angle change by the corresponding force to find the
 % angle change per applied Nm of force.
 
 CRS = GetToeCompliance(TARS, ToeMoments); % compliance at test corner
-CLS = GetToeCompliance(TALS, ToeMoments) ;% compliance at adjacent corner
-%% compliance function
+CLS = GetToeCompliance(TALS, ToeMoments); % compliance at adjacent corner
 
-function TC = GetToeCompliance(ang,Moment) % outputs degrees per Kg
-
-    TC = ang./Moment
-
-end
 
 %% 5. Plot the reults (should be linearish)
 
@@ -165,9 +131,38 @@ grid on
 
 
 
+%% displacement function
+function distance = GetDisplacements(D1in, D2in)
 
+    distance = D1in-D2in;
+    % convert to meters
+    distance = distance*.0000254; % from hundredths to meters
 
+end
 
+%% compliance function
 
+function TC = GetToeCompliance(ang,Moment) % outputs degrees per Kg
 
+    TC = ang./Moment;
 
+end
+
+%% Compliance angle function
+function theta = GetComplianceAng(Dispx,Dispy)
+
+    theta = rad2deg(atan(Dispy./Dispx)); 
+    % here we use ./ to divide each element of the matrix by the scalar. 
+end
+
+%% moment function
+% This function needs fed mass (Kg) and lever arm length (LAD)
+% We start by writing what the output will be, in this case "moments".
+% Next we define the name of our function "GetMoment" and put the
+% arguements it will need in  parenthesis (FKg, LAD)
+
+function moments = GetMoment(Kg, LAD) % get moments back in Nm
+
+% We then write the driving equation using the variables defined in the title. 
+    moments = Kg*LAD*9.81; % {kg*m*m/s^2) = Nm
+end
