2c3bdbb36fc418ec7db3c726188ca85236676500
khoile839 <kbl23@uakron.edu>
2025-11-18 19:40:29 -0500

    Create bumpSteer_testData.m

---
+++ MATLAB/Controls Stuff/bumpSteer_testData.m
@@ -0,0 +1,5 @@
+% Bump Steer Test Data Process
+% This code takes data from Bump Steer Testing and calculate bump steer
+% angle
+%Written by Carl and Khoi
+Excel = readtable("/Users/khoi/Downloads/200 Controls/Data Excels/bumpSteer_Eliminator.xlsx")
